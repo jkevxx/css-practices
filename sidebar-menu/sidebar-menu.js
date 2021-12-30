2f8a972e42b06e77186e67c63bc1426a767e89fe
Kevin Paez Gallardo <kevin.paez1010@gmail.com>
2021-12-29 21:18:13 -0600

    CSS Practices

---
+++ sidebar-menu/sidebar-menu.js
@@ -0,0 +1,22 @@
+
+// Agrega list o active list
+let list = document.querySelectorAll('.list');
+for (let i = 0; i < list.length; i++) {
+  list[i].onclick = function () {
+    let j = 0;
+    while (j < list.length) {
+      list[j++].className = 'list';
+    }
+    list[i].className = 'list active';
+  }
+}
+
+
+//Toggle
+let menuToggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation');
+menuToggle.onclick = function () {
+  menuToggle.classList.toggle('active');
+  navigation.classList.toggle('active');
+
+}
